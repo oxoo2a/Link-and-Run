f1a0faf7100ae212b4ca70f6b45d484d9bae4aff
Asysob <sturm@uni-trier.de>
2012-10-23 10:25:57 +0200

    Dereferencing a null pointer.

--- lar.c
+++ lar.c
@@ -5,8 +5,12 @@ int main ( int ac, char **av ) {
 	int some_int = 42;
 	printf("0x%016lx = address of main()\n",(long unsigned int) &main);
 	printf("0x%016lx = address of some_int\n",(long unsigned int) &some_int);
-	int *int_ptr = (int *) malloc(sizeof(int));
+	int *int_ptr;
+	int_ptr = (int *) malloc(sizeof(int));
 	*int_ptr = 42;
 	printf("0x%016lx = content of int_ptr\n",(long unsigned int) int_ptr);
+	
+	int_ptr = (int *) 0;
+	*int_ptr = 43;
 	return(0);
 }
